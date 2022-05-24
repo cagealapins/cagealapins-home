158669e304c1649658b288405c550d0f448083ca
Satsuki Akiba <anosatsuk124@gmail.com>
2022-05-25 00:36:38 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { Text, Box, Container, Heading, VStack, Link } from '@chakra-ui/react';
+
+const App = () => {
+    return (
+        <Box>
+            <Box
+                display="flex"
+                shadow="md"
+                borderWidth="6px"
+                padding="3px"
+                margin="12px"
+            >
+                <Heading as="h2">かじゃらぱんホーム</Heading>
+            </Box>
+            <Box margin="12px">
+                <Text fontSize="xl">ナレッジベース - Knowlege base</Text>
+                <Link
+                    fontSize="gl"
+                    href="https://wiki.cagealapins.space"
+                    isExternal
+                    color="blue"
+                >
+                    ページに飛ぶ
+                </Link>
+            </Box>
+        </Box>
+    );
+};
+
+export default App;
